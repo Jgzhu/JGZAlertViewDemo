caa15bd6591b78cae95f55d329da39ec8b201f90
jgzhu <251475337@qq.com>
2017-06-08 11:03:18 +0800

    初始化项目

---
+++ JGZAlertViewDemo/JGZAlertViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JGZAlertViewDemo
+//
+//  Created by 江贵铸 on 2017/6/8.
+//  Copyright © 2017年 江贵铸. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
